1d8d72523efa44b32e63f889b40980f69341c025
masashi16 <masashi.16@gmail.com>
2020-07-25 09:49:16 +0900

    Add files via upload

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.notebook_dir = '/project'
+c.NotebookApp.password = u'sha1:96e923bceea9:57ec957f5384f2a5501bb5024df7a5d09733eaed' # test
+c.NotebookApp.allow_root = True
+c.NotebookApp.open_browser = False
